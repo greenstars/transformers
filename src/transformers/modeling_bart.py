ade2a0addad38985dd143420499bb2da277b2670
Akila <akila@Cibins-MacBook-Pro.local>
2020-12-07 07:55:52 -0500

    fixing mask_fill

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -720,6 +720,7 @@ class Attention(nn.Module):
         if key_padding_mask is not None:  # don't attend to padding symbols
             attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
             reshaped = key_padding_mask.unsqueeze(1).unsqueeze(2)
+            reshaped[key_padding_mask.lt(1)] = 0
             attn_weights = attn_weights.masked_fill(reshaped, float("-inf"))
             attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
         attn_weights = F.softmax(attn_weights, dim=-1)
